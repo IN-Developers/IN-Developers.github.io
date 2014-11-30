9ed1cd1f8f51b3cee3cd78fd95b25e92e9daecbc
Yuuiti Okayama <yuuki.win7.56@live.jp>
2014-11-30 23:48:11 +0900

    new version HP

--- layout/js/base.js
+++ layout/js/base.js
@@ -1,32 +1,18 @@
-﻿/*
-window.onscroll = function () {
-    var header = document.getElementById('header');
-    var headerS = document.defaultView.getComputedStyle(header, '');
-    if (window.pageYOffset > 70 && headerS.display == 'none') {
-        header.style.top = '-30px';
-        header.style.display = 'block';
-        onScrollD();
-    } else if (window.pageYOffset < 70 && headerS.display != 'none') {
-        header.style.top = '0px';
-        onScrollU();
-        
-    }
-}
-
-NowScroll = false;
-function onScrollD() {
-    var header = document.getElementById('header');
-    if (parseInt(document.defaultView.getComputedStyle(header, '').top) < 0) {
-        setTimeout(onScrollD, 15);
-        header.style.top = parseInt(document.defaultView.getComputedStyle(header, '').top) + 2 + 'px';
-    }
-}
-function onScrollU() {
-    var header = document.getElementById('header');
-    if (parseInt(document.defaultView.getComputedStyle(header, '').top) > -30) {
-        setTimeout(onScrollU, 15);
-        header.style.top = parseInt(document.defaultView.getComputedStyle(header, '').top) - 1 + 'px';
-    } else {
-        header.style.display = 'none';
-    }
-}*/
+﻿
+$(function () {
+    $(document).on("click", "a[href ^= '#']", (function () {
+        var top = $(this.hash).offset().top - 60;
+        $("html, body").stop();
+        $("html, body").animate({ scrollTop: top }, 1500);
+        return false;
+    }));
+    $(document).scroll(function () {
+        if ($(document).scrollTop > 100 && paeInt($("#header").css("top")) < 0) {
+            $("#header").stop();
+            $("#header").animate({ top: 0 }, 1000);
+        } else if ($(document).scrollTop < 100 && parseInt($("#header").css("top")) > -40) {
+            $("#header").stop();
+            $("#header").animate({ top: -40 }, 1000);
+        }
+    })
+})
