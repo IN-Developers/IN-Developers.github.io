70e4d003f3693bebcdf2766f766dc8c0a77c45bf
Yuuiti Okayama <yuuki.win7.56@live.jp>
2014-11-30 23:53:40 +0900

    new version correction ver

--- layout/js/base.js
+++ layout/js/base.js
@@ -7,10 +7,10 @@ $(function () {
         return false;
     }));
     $(document).scroll(function () {
-        if ($(document).scrollTop > 100 && paeInt($("#header").css("top")) < 0) {
+        if ($(document).scrollTop() > 100 && paeInt($("#header").css("top")) < 0) {
             $("#header").stop();
             $("#header").animate({ top: 0 }, 1000);
-        } else if ($(document).scrollTop < 100 && parseInt($("#header").css("top")) > -40) {
+        } else if ($(document).scrollTop() < 100 && parseInt($("#header").css("top")) > -40) {
             $("#header").stop();
             $("#header").animate({ top: -40 }, 1000);
         }
