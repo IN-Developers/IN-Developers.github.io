9a835c42a935765d43dc349434dc3d194d64e4d5
Yuuiti Okayama <yuuki.win7.56@live.jp>
2014-12-01 00:22:15 +0900

    New style

--- layout/js/base.js
+++ layout/js/base.js
@@ -7,10 +7,10 @@ $(function () {
         return false;
     }));
     $(document).scroll(function () {
-        if ($(document).scrollTop() > 100 && parseInt($("#header").css("top")) < 0) {
+        if ($(document).scrollTop() > 150 && parseInt($("#header").css("top")) < 0) {
             $("#header").stop();
             $("#header").animate({ top: 0 }, 1000);
-        } else if ($(document).scrollTop() < 100 && parseInt($("#header").css("top")) > -50) {
+        } else if ($(document).scrollTop() < 150 && parseInt($("#header").css("top")) > -50) {
             $("#header").stop();
             $("#header").animate({ top: -50 }, 1000);
         }
