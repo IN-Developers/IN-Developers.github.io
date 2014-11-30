2ee5a0abdea581cc0916f1728b00a32beaf0d5ba
Yuuiti Okayama <yuuki.win7.56@live.jp>
2014-12-01 01:40:14 +0900

    New script

--- layout/js/base.js
+++ layout/js/base.js
@@ -3,16 +3,16 @@ $(function () {
     $(document).on("click", "a[href ^= '#']", (function () {
         var top = $(this.hash).offset().top - 60;
         $("html, body").stop();
-        $("html, body").animate({ scrollTop: top }, 1500);
+        $("html, body").animate({ scrollTop: top }, 1000);
         return false;
     }));
     $(document).scroll(function () {
         if ($(document).scrollTop() > 150 && parseInt($("#header").css("top")) < 0) {
             $("#header").stop();
-            $("#header").animate({ top: 0 }, 1000);
+            $("#header").animate({ top: 0 }, 700);
         } else if ($(document).scrollTop() < 150 && parseInt($("#header").css("top")) > -50) {
             $("#header").stop();
-            $("#header").animate({ top: -50 }, 1000);
+            $("#header").animate({ top: -50 }, 700);
         }
     })
 })
