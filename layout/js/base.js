0af4f0a08e9a9385053395cd293fb7d5b02216e9
Yuuiti Okayama <yuuki.win7.56@live.jp>
2014-12-03 02:55:41 +0900

    New design homepage

--- layout/js/base.js
+++ layout/js/base.js
@@ -1,12 +1,33 @@
 ﻿
 $(function () {
+    $("#menu #link a").hover(function () {
+        $(this).css("background-color", "#F8F8F8");
+    }, function () {
+        $(this).css("background-color", "");
+    });
     $(document).on("click", "a[href ^= '#']", (function () {
         var top = $(this.hash).offset().top - 60;
         $("html, body").stop();
         $("html, body").animate({ scrollTop: top }, 1000);
         return false;
     }));
+
+    // GET HTML - Ajax
+    $(document).on("click", "a[href ^= './']", function () {
+        $.ajax({
+            dataType: "html", url: $(this).attr("href"), success: function (data) {
+                if ($("#main", data).length < 1) {
+                    $("#main").html(data.html());
+                    return false;
+                }
+                $("#main").html($("#main", data).html());
+            }, error: function () { $("#main").html("<p>Error</p>"); }
+        });
+        return false;
+    });
+
     $(document).scroll(function () {
+        return false;
         if ($(document).scrollTop() > 150 && parseInt($("#header").css("top")) < 0) {
             $("#header").stop();
             $("#header").animate({ top: 0 }, 700);
@@ -14,5 +35,6 @@ $(function () {
             $("#header").stop();
             $("#header").animate({ top: -50 }, 700);
         }
-    })
+    });
+
 })
