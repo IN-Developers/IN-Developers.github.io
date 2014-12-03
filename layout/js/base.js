4a5046df8bc8f335f130b90b47df8f786473ea02
Yuuiti Okayama <yuuki.win7.56@live.jp>
2014-12-04 05:04:15 +0900

    New script(fixed animate)

--- layout/js/base.js
+++ layout/js/base.js
@@ -1,5 +1,5 @@
-﻿
-$(function () {
+﻿$(function () {
+
     $("#menu #link a").hover(function () {
         $(this).css("background-color", "#F8F8F8");
     }, function () {
@@ -13,22 +13,23 @@ $(function () {
     }));
 
     // GET HTML - Ajax
-    $(document).on("click", "a[href ^= './'], a[href ^= 'http://github.dev003.net/']", function () {
-        $("#main").animate({ opacity: 0 }, 300);
-        $.ajax({
-            dataType: "html", url: $(this).attr("href"), success: function (data) {
-                if ($("#main", data).length < 1) {
-                    $("#main").html(data.html());
-                    return false;
-                }
-                $("#main").html($("#main", data).html());
-            }, error: function () { $("#main").html("<p>Error</p>"); },
-            complete: function () { $("#main").animate({ opacity: 1 }, 300) }
+    $(document).on("click", "a[href ^= './'], a[href ^= '../'], a[href ^= 'http://github.dev003.net/']", function () {
+        $("#main").animate({ opacity: 0, width: 500 }, 200, function () {
+            $.ajax({
+                dataType: "html", url: $(this).attr("href"), success: function (data) {
+                    if ($("#main", data).length < 1) {
+                        $("#main").html(data.html());
+                        return false;
+                    }
+                    $("#main").html($("#main", data).html());
+                }, error: function () { $("#main").html("<p>Error</p>"); },
+                complete: function () { $("#main").animate({ opacity: 1, width: 650 }, 200) }
+            });
         });
         history.pushState(null, "", $(this).attr("href"));
         return false;
     });
-    $(document).on("popstate", function () {
+    $(window).on("popstate", function () {
         location.replace(location.href);
     });
 
